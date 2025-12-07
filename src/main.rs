dcb161d0eb8d2c5244566859565b01daa35963b0
DragonDev22 <dragon22gaming.yt@gmail.com>
2025-12-07 21:55:43 +1030

    implement 'echo' builtin

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::exit;
+use std::process;
+
+enum Cmd {
+    Echo(String),
+    Exit,
+    Other(String),
+}
 
 fn main() {
-    let exit: bool = false;
+    let mut exit: bool = false;
 
     while !exit {
         let command = get_input();
 
-        // Evaluate:
-        evaluate(&command);
+        let task = evaluate(&command);
 
-        // If command doesn't exist:
-        error(command);
+        exit = execute(task);
     }
 }
 
@@ -28,10 +32,28 @@ fn error(command: String) {
     println!("{}: command not found", command)
 }
 
-fn evaluate(command: &String) {
-    match command.as_str() {
-        "exit" => exit(0),
-        "quit" => exit(0),
-        _ => return,
+fn evaluate(command: &String) -> Cmd {
+    let mut task;
+    let first_word = first_word(&command);
+    match first_word {
+        "exit" => task = Cmd::Exit,
+        "quit" => task = Cmd::Exit,
+        "echo" => task = Cmd::Echo(command.split_once(" ").unwrap().1.to_string()),
+        _ => task = Cmd::Other(first_word.to_string()),
     }
+    task
+}
+
+fn execute(task: Cmd) -> bool {
+    let mut exit = false;
+    match task {
+        Cmd::Exit => exit = true,
+        Cmd::Echo(value) => println!("{}", value),
+        Cmd::Other(value) => println!("{}: command not found", value),
+    }
+    exit
+}
+
+fn first_word(s: &str) -> &str {
+    s.split_whitespace().next().unwrap_or("")
 }
