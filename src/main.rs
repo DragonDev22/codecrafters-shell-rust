c71a431894481c1adb4969de78e0ec52724299af
DragonDev22 <dragon22gaming.yt@gmail.com>
2025-12-07 21:19:50 +1030

    loop the program

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,26 @@
 use std::io::{self, Write};
 
 fn main() {
+    let exit: bool = false;
+
+    while !exit {
+        let command = get_input();
+
+        // Evaluate:
+
+        // If command doesn't exist:
+        error(command);
+    }
+}
+
+fn get_input() -> String {
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim())
+    return command.trim().to_string();
+}
+
+fn error(command: String) {
+    println!("{}: command not found", command)
 }
